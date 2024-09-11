ce447ea5fdaaaaf5cfe559b89d5b56ecb437f4ce
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-09-11 05:21:29 +0100

    Setting up.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import { initializeApp } from "firebase/app";
+
 import "./App.css";
 
+const firebaseConfig = {};
+
+const app = initializeApp(firebaseConfig);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="container mx-auto px-4">
+      <p>It works!!!</p>
+    </div>
+  );
 }
 
 export default App;
