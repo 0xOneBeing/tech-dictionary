32657a5c87e1caf4c0d2f69e7e251a5ec5928db7
Sunday Oruwhone <sundayoruwhone@gmail.com>
2024-09-17 21:55:01 +0100

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { initializeApp } from "firebase/app";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
 import Home from "./pages/Home/Home";
 import SubmitWord from "./pages/SubmitWord/SubmitWord";
 import Admin from "./pages/Admin/Admin";
